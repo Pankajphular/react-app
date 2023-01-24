826d1d835de16acc1d981fb8202850f47b938a4c
Pankajphular <phularpankaj1@gmail.com>
2023-01-24 22:16:59 +0530

    fix: fix description

---
+++ src/release.config.js
@@ -0,0 +1,5 @@
+module.exports={
+    branch: "master",
+    repositoryUrl: "https://github.com/Pankajphular/react-app/",
+    Plugin: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
+}
